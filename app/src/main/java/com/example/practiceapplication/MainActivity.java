c6bad4a7b0dcd233cf7baefa34419596608d6fc2
nafitus <nafisul.nazrul@metropolia.fi>
2021-11-23 13:46:48 +0200

    Moro

---
+++ app/src/main/java/com/example/practiceapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.practiceapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Harjoitusäppi");
+                Toast.makeText(getApplicationContext(), "Harjoitusäppi", Toast.LENGTH_LONG);
+
+            }
+        });
+    }
+}
